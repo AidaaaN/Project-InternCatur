94b3510ff152913ec826b5c3fe2dd40c9c89a993
MushabAli <mushabalimaulana71@gmail.com>
2024-07-15 13:21:59 +0700

    migration

---
+++ database/migrations/2024_07_15_023929_surat_nda.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    protected $table = 'surat_nda';
+    public function up(): void
+    {
+        //
+        Schema::create($this->table,function(Blueprint $table){
+            $table->integer('id_surat_nda',true,false)->nullable(false);
+            $table->integer('id_user',false,false)->nullable(false);
+            $table->vachar('kode_surat',100)->nullable(false);
+            $table->text('isi_surat')->nullable(false);
+            //foreign key
+            $table->foreign('id_user',)->on('user')->references('id_user')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists($this->table);
+    }
+};
