bffec6a4bf7b14329683afd44e803d3c34739c97
mutiaraayur <mutiaraayur5@gmail.com>
2024-07-22 10:39:21 +0700

    app js sama app css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,19 @@
 import './bootstrap';
 
+
+//jqueryModule loader
+//npm install jquery
 import jQuery from 'jquery';
 window.$ = window.jQuery =jQuery;
 
+
+//Sweetalert2 loader
+//npm install swetalert2
 import Swal from 'sweetalert2';
 window.Swal = Swal
+
+import DataTable from 'datatables.net-bs5';
+window.DataTable = DataTable;
+
+import select2 from 'select2';
+select2();
