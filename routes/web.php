bdf7e7ddcde4ad0bab728bcc831777d7102bc5c1
MushabAli <mushabalimaulana71@gmail.com>
2024-08-09 10:27:56 +0700

    arsip

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\arsipController;
 use App\Http\Controllers\dashboardController;
 use App\Http\Controllers\loginController;
+use App\Http\Controllers\surat_masukController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,3 +20,6 @@ Route::prefix('/login')->group(function(){
 Route::prefix('/dashboard')->middleware('admin')->group(function(){
     Route::get('/',[dashboardController::class ,'index'])->name('dashboard');
 });
+
+Route::get('/arsip', [arsipController::class,'index'])->name('arsip.index');
+//Route::get('/suratMasuk', [surat_masukController::class,'index'])->name('surat_masuk.index');
