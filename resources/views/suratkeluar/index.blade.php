0d6462de87757e0c8e3452fe2379ad276d25adcf
nadinemodd <naddneeputri@gmail.com>
2024-08-13 15:26:25 +0700

    suratkeluar

---
+++ resources/views/suratkeluar/index.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Surat Keluar</title>
+    <link rel="stylesheet" href="style3.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"/>
+</head>
+
+<body>
+    <div class="table">
+        <div class="table_header">
+            <div>
+                <input placeholder="Search"/>
+                <button class="cari">🔎 Search</button>
+                <button class="tambah">+ Tambah</button> 
+
+                <div class="table_section">
+                    <h1>Data Surat Keluar</h1>
+                </div>
+            </div>
+        </div>
+            <body>
+                <main class="table">
+                    <section class="table__header">
+
+                        <div class="table_section">
+                            <h2>3 Bulan Terakhir</h2>
+                        </div>
+                    </section>
+                    
+            
+                    <section class="table__body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th> No </th>
+                                <th> Nama Surat </th>
+                                <th> Nomor Surat </th>
+                                <th> Tanggal Surat </th>
+                                <th> Tujuan Surat </th>
+                                <th> Jenis Surat </th>
+                                <th> Keterangan </th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td> 1 </td>
+                                <td> Merge </td>
+                                <td> 47.92/S87256 </td>
+                                <td> 01/08/2024 </td>
+                                <td> Pt Nas </td>
+                                <td> Kerjasama </td>
+                                <td> Terkirim </td>
+                                <td>
+                                    <button><i class="fa-solid fa-pen-to-square"   ></i></button>
+                                  <button><i class="fa-solid fa-trash"></i></button>
+
+                                </td>
+                            </tr>
+                            <tr>
+                                <td> 1 </td>
+                                <td> Merge </td>
+                                <td> 47.92/S87256 </td>
+                                <td> 01/08/2024 </td>
+                                <td> Pt Nas </td>
+                                <td> Kerjasama </td>
+                                <td> Terkirim </td>
+                                <td>
+                                    <button><i class="fa-solid fa-pen-to-square"   ></i></button>
+                                     <button><i class="fa-solid fa-trash"></i></button>
+
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </body>
+            
+        </div>
+    </div>
+</body>
+<footer>
+
+</footer>
+</html>
