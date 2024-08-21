fdb922fc78b5e6b6c05aa12ef5991f87d1ae615a
magdalenaannabella <iamannabelle295@gmail.com>
2024-08-21 15:21:28 +0700

    data tambah suratmasuk

--- resources/views/suratmasuk/index.blade.php
+++ resources/views/suratmasuk/index.blade.php
@@ -134,11 +134,34 @@
         $('.btnTambahFile').on('click', function(a) {
             a.preventDefault();
             const modalForm = document.getElementById('modalForm');
-            modalForm.addEventListener('shown.bs.modal',function(event){
-                event.preventDefault();
-                event.stopImmediatePropagation();
+            modalForm.addEventListener('shown.bs.modal', function(eventTambah) {
+                eventTambah.preventDefault();
+                eventTambah.stopImmediatePropagation();
                 const link = event.relatedTarget.getAttribute('attr-href');
-                
+
+                axios.get(link).then(response => {
+                    $("#modalForm .modal-body").html(response.data);
+                    $('.autoDropdownSurat').select2({
+                        placeholder: 'Pilih barang yang ingin dibeli',
+                        theme: 'bootstrap-5',
+                        cache: true,
+                        dropdownParent: $('#modalForm'),
+                        ajax: {
+                            dataType: 'json',
+                            processResults: function(data) {
+                                $.each(data, function(i, d) {
+                                    //i = Iterasi ke n
+                                    //d = data dari iterasi i
+                                    data[i]['text'] = d.nama_surat;
+                                    data[i]['id'] = d.id_surat;
+                                });
+                                return {
+                                    results: data
+                                }
+                            }
+                        }
+                    });
+                });
             })
         })
     </script>
